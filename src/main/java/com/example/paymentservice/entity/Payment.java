18671eba7584bbb7c518653e54e8bcc990ce2178
laserany <dotachessfan@outlook.com>
2021-01-30 22:21:28 -0600

    first commit

---
+++ src/main/java/com/example/paymentservice/entity/Payment.java
@@ -0,0 +1,25 @@
+package com.example.paymentservice.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "PAYMENT_TB")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Payment {
+    @Id
+    @GeneratedValue
+    private Long paymentId;
+    private String paymentStatus;
+    private String transactionId;
+    private Long orderId;
+    private Integer amount;
+}
